e423923418da218e009eb5e3224e322e9ef8fecc
Denialves <denilson_deny@hotmail.com>
2024-09-03 18:10:51 -0300

    media

---
+++ Operadores/src/br/com/tarefa/mod8/Operadores.java
@@ -0,0 +1,24 @@
+package br.com.tarefa.mod8;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// declaração das variáveis 
+		double nota1 = 3;
+		double nota2 = 4;
+		double nota3 = 5;
+		double nota4 = 6;
+		
+		
+	
+		// cálculo da média (Processamento)
+		double media = (nota1 + nota2 + nota3 + nota4)/4;
+		
+		// exibição de média (Saida)
+		System.out.printf("A média é:" + " " + media);
+		
+		
+
+	}
+
+}
